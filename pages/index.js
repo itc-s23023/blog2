cf9623aabf68d170f6ccd619d9b1c307415fd327
itc-s23023 <s23023@std.it-college.ac.jp>
2024-01-23 06:04:26 +0000

    finished chapter1.7 p40
    Make header,body and footer.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
 const Home = () => {
-  return <h1>CUBE</h1>
+  return (
+    <>
+      <header>HEADER</header>
+
+      <main>
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットしていくサイト</p>
+        </div>
+      </main>
+      <footer>FOOTER</footer>
+    </>
+  )
 }
-export default Home()
+export default Home
