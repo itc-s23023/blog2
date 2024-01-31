37a4e051f870d8799354f6bde36f567284a72a97
itc-s23023 <s23023@std.it-college.ac.jp>
2024-01-31 02:20:32 +0000

    finished chapter6.4
    Set meta dictionary.

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,13 @@
+import Meta from 'components/meta'
+import Container from 'components/container'
 import Hero from 'components/hero'
 const Blog = () => {
-  return <Hero title='Blog' subtitle='Recent Posts' />
+  return (
+    <Container>
+      <Meta pageTitle='ブログ' />
+      <Hero title='Blog' subtitle='Recent Posts' />
+    </Container>
+  )
 }
 export default Blog
+//  117
