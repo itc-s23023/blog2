0f7cbe903ed639f084234145226eeeb7c14d33a3
itc-s23023 <s23023@std.it-college.ac.jp>
2024-01-31 01:20:08 +0000

    finished chapter5.7 p171
    Make Font Awesome usable.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import 'styles/globals.css'
 import Layout from 'components/layout'
 
+//  Font Awesomeの設定
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
