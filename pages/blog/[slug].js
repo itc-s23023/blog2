8979fe79ada7c3d0b8e228b95124dd1305982fcd
itc-s23023 <s23023@std.it-college.ac.jp>
2024-02-02 02:27:05 +0000

    finished chapter9.3 p256
    Make all article pages.

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,4 +1,4 @@
-import { getPostBySlug } from 'lib/api'
+import { getPostBySlug, getAllSlugs } from 'lib/api'
 import { extractText } from 'lib/extract-text'
 import Meta from 'components/meta'
 import Container from 'components/container'
@@ -62,8 +62,9 @@ const Schedule = ({
 }
 
 export async function getStaticPaths () {
+  const allSlugs = await getAllSlugs()
   return {
-    paths: ['/blog/schedule', '/blog/music', '/blog/micro'],
+    paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
     fallback: false
   }
 }
