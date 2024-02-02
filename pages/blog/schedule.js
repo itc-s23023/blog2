b04fbe4dd6a23dededbae42070f29b2c1460d4a0
itc-s23023 <s23023@std.it-college.ac.jp>
2024-02-02 02:05:21 +0000

    finished chapter9.1 p251
    Set Approximate image and Blur image.

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -12,6 +12,8 @@ import {
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/legacy/image'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 const Schedule = ({
   title,
   publish,
@@ -40,6 +42,8 @@ const Schedule = ({
             height={eyecatch.height}
             sizes='(min-width: 1152px) 1152px, 100vw'
             priority
+            placeholder='blur'
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
         <TwoColumn>
@@ -58,16 +62,19 @@ const Schedule = ({
 }
 
 export async function getStaticProps () {
-  const slug = 'schedule'
+  const slug = 'micro'
   const post = await getPostBySlug(slug)
   const description = extractText(post.content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
 
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch,
       categories: post.categories,
       description
     }
