5d74b2a054b9f15cb390d73e9a2054fb353e057e
itc-s23023 <s23023@std.it-college.ac.jp>
2024-02-02 03:35:49 +0000

    finished chapter9.6 p275
    Add eyecatch iamge to list of articles.

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,8 @@ import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 const Blog = ({ posts }) => {
   return (
     <Container>
@@ -12,14 +14,42 @@ const Blog = ({ posts }) => {
     </Container>
   )
 }
+/*
 const getStaticProps = async () => {
   const posts = await getAllPosts()
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
   return {
     props: {
       posts
     }
   }
 }
+*/
+const getStaticProps = async () => {
+  const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!Object.prototype.hasOwnProperty.call(post, 'eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 export default Blog
 export { getStaticProps }
 //  117
