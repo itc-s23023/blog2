7b352087ed8e287b6640d79d0ff0caa69640a08c
itc-s23023 <s23023@std.it-college.ac.jp>
2024-02-06 02:25:06 +0000

    finished chapter9.10 p289
    Show list of articles in category pages.

--- pages/blog/category/[slug].js
+++ pages/blog/category/[slug].js
@@ -1,11 +1,15 @@
-import { getAllcategories } from 'lib/api'
+import { getAllcategories, getAllPostsByCategory } from 'lib/api'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 
-const Category = ({ name }) => {
+const Category = ({ name, posts }) => {
   return (
     <Container>
       <PostHeader title={name} subtitle='Blog Category' />
+      <Posts posts={posts} />
     </Container>
   )
 }
@@ -22,10 +26,19 @@ const getStaticProps = async context => {
   const catSlug = context.params.slug
   const allCats = await getAllcategories()
   const cat = allCats.find(({ slug }) => slug === catSlug)
+  const posts = await getAllPostsByCategory(cat.id)
+  for (const post of posts) {
+    if (!Object.prototype.hasOwnProperty.call(post, 'eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
 
   return {
     props: {
-      name: cat.name
+      name: cat.name,
+      posts
     }
   }
 }
