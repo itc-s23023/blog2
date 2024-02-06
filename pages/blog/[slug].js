d919f1e5e30018d911a5a1490374c9a9f500985f
itc-s23023 <s23023@std.it-college.ac.jp>
2024-02-06 01:05:37 +0000

    finished chapter9.8 p281
    Available to use blur in images while moving by Pagination.

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -39,6 +39,7 @@ const Schedule = ({
         <PostHeader title={title} subtitle='Blog Article' publish={publish} />
         <figure>
           <Image
+            key={eyecatch.url}
             src={eyecatch.url}
             alt=''
             layout='responsive'
